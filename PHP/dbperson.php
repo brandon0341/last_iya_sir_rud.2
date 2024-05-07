6c822da0e36f49dd30ebdec1c95bb826b198eaf4
brandon0341 <itsmebrandon4@gmail.com>
2024-05-07 20:12:05 +0800

    commit

---
+++ PHP/dbperson.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/EXAM/CSS/person.css">
+</head>
+<body>
+    <div class="person">
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg" alt="">
+
+        <div class="infor">
+        <label for="firstname">Firstname:</label><br>
+        <input type="text" id="firstname" name="firstname"><br>
+        
+        <label for="lastname">Lastname:</label><br>
+        <input type="text" id="lastname" name="lastname"><br>
+        
+        <label for="repassword">Email :</label><br>
+        <input type="password" id="repassword" name="repassword" required><br>
+
+        <label for="contact">Mobile No:</label><br>
+        <input type="text" id="contact" name="contact"><br>
+
+        <label for="password">Address:</label><br>
+        <input type="password" id="password" name="password" required><br>
+        
+        
+        <button>Cancel</button>
+        <button>Update</button>
+        </div>
+</html>
--- PHP/prof.php
+++ PHP/prof.php
@@ -13,7 +13,7 @@
             <a href="/EXAM/PHP/home.php"><img class="row" src="/EXAM/PIC/arrow.png" alt=""></a>
         </div>
         <div class="prof">
-            <a href=""><img src="/EXAM/PIC/user.webp" alt="">Personal Account </a>
+            <a href="/EXAM/PHP/dbperson.php"><img src="/EXAM/PIC/user.webp" alt="">Personal Account </a>
             <a href="/EXAM/PHP/acc.php"><img src="/EXAM/PIC/user.webp" alt="">Accounts</a>
             <a href="https://dbdiagram.io/d/65d4956bac844320ae92002e"><img src="/EXAM/PIC/dia.jpg" alt="">Diagram</a>
             <a href="/EXAM/PHP/tim.php"><img src="/EXAM/PIC/tim.jpg" alt="">BoardMembers</a>
